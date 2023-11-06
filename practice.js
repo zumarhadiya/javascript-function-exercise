346340cb136f3312149358d39ae34c681712eb26
zumarhadiya <zumarhadiya2@gamail.com>
2023-11-07 02:16:57 +0500

    my repo

---
+++ practice.js
@@ -0,0 +1,58 @@
+// Write a program that returns your age in days. (take age as an input)
+// let age=prompt("enter an age to convert it in days")
+// function ageImDays(age) {
+//    return age*365
+// }
+// console.log(ageImDays(age))
+
+
+// Create a function that takes a number as an argument, increments the number
+//  by +1 and returns the result. (take number as an input)
+// let num=prompt("write a number for increment");
+// num=Number.parseInt(num)
+// function ageImDays(num) {
+//    return age+1
+// }
+// console.log(ageImDays(num))
+
+
+// Create a function that takes a number as an argument, decrements the number by -1 and returns the result.
+// let num=5;
+// num=Number.parseInt(num)
+// function ageImDays(num) {
+//    return num-1
+// }
+// console.log(ageImDays(num))
+
+
+// Create a function that takes a number and return square of a number
+// function square(a) {
+//     return a*a    
+// }
+// console.log(square(6))
+
+
+// Create a function that tells current time in Y-m-d format.
+// function currentTime() {
+//     let dt= new Date();
+//     let yr = dt.getFullYear();
+//     let mh = String(dt.getMonth()+1).padStart(2,'0');
+//     let de= String(dt.getDate()).padStart(2,'0');
+// return `${yr}-${mh}-${de}`
+// }
+// let ymd=currentTime();
+// console.log(ymd);
+
+
+// Create a function that tells current time from Date object in H:i:s format?
+// function currentTime() {
+//         let his = new Date();
+//         let hr = his.getHours();
+//         let min = his.getMinutes();
+//         let sec = his.getSeconds();
+//     return `${hr}:${min}:${sec}`
+//     }
+//     let hisFormat=currentTime();
+//     console.log(hisFormat);
+
+
